6803f9884c90f2305adbc60bdf5a640f9b4ccc7c
bsuc <bsuc@student.42.fr>
2024-06-28 22:51:09 +0100

    ok

--- srcs/requirements/mariadb/conf/test2.sh
+++ srcs/requirements/mariadb/conf/test2.sh
@@ -2,15 +2,18 @@
 
 mysqld &
 
+sleep 2
+
 echo "CREATE DATABASE $MYSQL_DATABASE;" | mysql
-echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'"; | mysql
+echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" | mysql
 echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY 'WPpassw0rd' WITH GRANT OPTION;" | mysql
 echo "GRANT ALL ON $MYSQL_DATABASE.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' WITH GRANT OPTION;" | mysql
 echo "FLUSH PRIVILEGES;" | mysql
 
+# kill $(cat /var/run/mysqld/mysqld.pid)
 
+# bash
 
-kill $(cat /var/run/mysqld/mysqld.pid)
+mysqladmin shutdown
 
-mysqld
-
+mysqld_safe
