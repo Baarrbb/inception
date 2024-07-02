65d8078ee39ebcf1393af545ab4cbff390fde3af
bsuc <bsuc@student.42.fr>
2024-07-02 20:09:18 +0100

    ok

--- srcs/requirements/wordpress/tools/entry.sh
+++ srcs/requirements/wordpress/tools/entry.sh
@@ -2,7 +2,7 @@
 
 path=/var/www/html
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+curl -s -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
@@ -10,17 +10,17 @@ rm -f $path/index.nginx-debian.html
 
 if [ ! "$(ls -A  $path)" ]; then
 
-	wp core download --allow-root --path=$path
+	wp core download --allow-root --path=$path --quiet
 
 	# Genere le wp-config.php
 	cd $path
-	wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWD --dbhost=$WP_HOST_DB
+	wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWD --dbhost=$WP_HOST_DB --quiet
 
 	# CREATE ADMIN
 	wp core install --allow-root --path=$path --url=$DOMAIN_NAME --title="Inception" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWD --admin_email=$WP_ADMIN_EMAIL
 
 	# CREATE USER
-	wp user create --allow-root --path=$path $WP_USER $WP_EMAIL --user_pass=$WP_PASSWD --role=$WP_USER_ROLE
+	wp user create --allow-root --path=$path $WP_USER $WP_EMAIL --user_pass=$WP_PASSWD --role=$WP_USER_ROLE --quiet
 
 fi
 
