8b60b9b0f3ccdf5041c5908fc751b26e5609b0cc
bsuc <bsuc@student.42.fr>
2024-07-02 19:41:23 +0100

    ok

--- srcs/requirements/wordpress/tools/entry.sh
+++ srcs/requirements/wordpress/tools/entry.sh
@@ -6,16 +6,22 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-wp core download --allow-root --path=$path
+rm -f $path/index.nginx-debian.html
 
-# Genere le wp-config.php
-cd $path
-wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWD --dbhost=$WP_HOST_DB
+if [ ! "$(ls -A  $path)" ]; then
 
-# CREATE ADMIN
-wp core install --allow-root --path=$path --url=$DOMAIN_NAME --title="Inception" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWD --admin_email=$WP_ADMIN_EMAIL
+	wp core download --allow-root --path=$path
 
-# CREATE USER
-wp user create --allow-root --path=$path $WP_USER $WP_EMAIL --user_pass=$WP_PASSWD --role=$WP_USER_ROLE
+	# Genere le wp-config.php
+	cd $path
+	wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWD --dbhost=$WP_HOST_DB
+
+	# CREATE ADMIN
+	wp core install --allow-root --path=$path --url=$DOMAIN_NAME --title="Inception" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWD --admin_email=$WP_ADMIN_EMAIL
+
+	# CREATE USER
+	wp user create --allow-root --path=$path $WP_USER $WP_EMAIL --user_pass=$WP_PASSWD --role=$WP_USER_ROLE
+
+fi
 
 php-fpm8.2 -F -y /etc/php/8.2/fpm/php-fpm.conf
