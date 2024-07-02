5569f9e40e85e46cff601e5c132906e27c00fbd0
bsuc <bsuc@student.42.fr>
2024-07-02 15:49:35 +0100

    ok

--- srcs/requirements/wordpress/tools/entry.sh
+++ srcs/requirements/wordpress/tools/entry.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 
+path=/var/www/html
+
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 mv wp-cli.phar /usr/local/bin/wp
 
-wp core download --allow-root --path=/var/www/html
+wp core download --allow-root --path=$path
 
 # Genere le wp-config.php
-cd /var/www/html
+cd $path
 wp config create --allow-root --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWD --dbhost=$WP_HOST_DB
 
 # CREATE ADMIN
-wp core install --allow-root --path=/var/www/html --url=$DOMAIN_NAME --title="Inception" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWD --admin_email=$WP_ADMIN_EMAIL
+wp core install --allow-root --path=$path --url=$DOMAIN_NAME --title="Inception" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWD --admin_email=$WP_ADMIN_EMAIL
 
 # CREATE USER
-wp user create --allow-root --path=/var/www/html $WP_USER $WP_EMAIL --user_pass=$WP_PASSWD --role=$WP_USER_ROLE
+wp user create --allow-root --path=$path $WP_USER $WP_EMAIL --user_pass=$WP_PASSWD --role=$WP_USER_ROLE
 
 php-fpm8.2 -F -y /etc/php/8.2/fpm/php-fpm.conf
