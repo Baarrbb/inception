796c26c8d885b95964e1ec580818c01584942e29
bsuc <bsuc@student.42.fr>
2024-08-08 19:16:15 +0100

    tjr erreur 403 et 502 quand trop rapide

--- srcs/requirements/mariadb/tools/setup_db.sh
+++ srcs/requirements/mariadb/tools/setup_db.sh
@@ -4,6 +4,8 @@ sed -i 's/bind-address\s*=\s*127\.0\.0\.1/bind-address = 0.0.0.0/' /etc/mysql/ma
 
 service mariadb start
 
+sleep 2
+
 echo "CREATE DATABASE $DB_NAME;" | mariadb
 echo "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWD';" | mariadb
 echo "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWD' WITH GRANT OPTION;" | mariadb
