6ed574e9f76c1aac9927939d98dd573548fdb0d2
bsuc <bsuc@student.42.fr>
2024-08-19 00:00:14 +0100

    Grafana et secrets ok ??

---
+++ srcs/requirements/bonus/grafana/tools/start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+
+GRAF_ADMIN=$(cat /run/secrets/my_graf_admin)
+GRAF_PASSWD=$(cat /run/secrets/my_graf_passwd)
+
+sed -i "s/;admin_user = admin/admin_user = ${GRAF_ADMIN}/" /etc/grafana/grafana.ini
+sed -i "s/;admin_password = admin/admin_password = ${GRAF_PASSWD}/" /etc/grafana/grafana.ini
+
+
+grafana-server --homepath=/usr/share/grafana --config=/etc/grafana/grafana.ini
